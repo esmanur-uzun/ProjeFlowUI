b7e6b919d9031d5432905d200414a2a2411f86bd
esmanur-uzun <esmabitmez@gmail.com>
2024-09-02 15:32:56 +0300

    worked on sidebar

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -1,57 +1,52 @@
+"use client"
+import Link from "next/link";
+import { PanelsTopLeft } from "lucide-react";
 
-// import { cn } from "https://shadcn-ui.veryfront.com/@components/lib/utils";
-import { Button } from "../ui/button";
-import { ScrollArea } from "../ui/scroll-area";
+import { cn } from "@/lib/utils";
+import { useStore } from "@/hooks/use-store";
+import { useSidebarToggle } from "@/hooks/use-sidebar-toggle";
+import { Button } from "@/components/ui/button";
+import { Menu } from "./menu";
+import { SidebarToggle } from "./sidebar-toggle";
+
 export function Sidebar() {
+  const sidebar = useStore(useSidebarToggle, (state) => state);
+
+  if (!sidebar) return null;
+
   return (
-    <div className={"pb-12"}>
-      <div className="space-y-4 py-4">
-        <div className="px-4 py-2">
-          <h2 className="mb-2 px-2 text-lg font-semibold tracking-tight">
-            Discover
-          </h2>
-          <div className="space-y-1">
-            <Button
-              variant="secondary"
-              size="sm"
-              className="w-full justify-start"
+    <aside
+      className={cn(
+        "fixed top-0 left-0 z-20 h-screen -translate-x-full lg:translate-x-0 transition-[width] ease-in-out duration-300",
+        sidebar?.isOpen === false ? "w-[90px]" : "w-72"
+      )}
+    >
+      <SidebarToggle isOpen={sidebar?.isOpen} setIsOpen={sidebar?.setIsOpen} />
+      <div className="relative h-full flex flex-col px-3 py-4 overflow-y-auto shadow-md dark:shadow-zinc-800">
+        <Button
+          className={cn(
+            "transition-transform ease-in-out duration-300 mb-1",
+            sidebar?.isOpen === false ? "translate-x-1" : "translate-x-0"
+          )}
+          variant="link"
+          asChild
+        >
+          <Link href="/dashboard" className="flex items-center gap-2">
+            <PanelsTopLeft className="w-6 h-6 mr-1" />
+            <h1
+              className={cn(
+                "font-bold text-lg whitespace-nowrap transition-[transform,opacity,display] ease-in-out duration-300",
+                sidebar?.isOpen === false
+                  ? "-translate-x-96 opacity-0 hidden"
+                  : "translate-x-0 opacity-100"
+              )}
             >
-              Listen Now
-            </Button>
-            <Button variant="ghost" size="sm" className="w-full justify-start">
-              Browse
-            </Button>
-            <Button variant="ghost" size="sm" className="w-full justify-start">
-              Radio
-            </Button>
-          </div>
-        </div>
-        <div className="px-4 py-2">
-          <h2 className="mb-2 px-2 text-lg font-semibold tracking-tight">
-            Library
-          </h2>
-          <div className="space-y-1">
-            <Button variant="ghost" size="sm" className="w-full justify-start">
-              Playlists
-            </Button>
-            <Button variant="ghost" size="sm" className="w-full justify-start">
-              Songs
-            </Button>
-            <Button variant="ghost" size="sm" className="w-full justify-start">
-              Made for You
-            </Button>
-            <Button variant="ghost" size="sm" className="w-full justify-start">
-              Artists
-            </Button>
-            <Button variant="ghost" size="sm" className="w-full justify-start">
-              Albums
-            </Button>
-          </div>
-        </div>
-       
+              Brand
+            </h1>
+          </Link>
+        </Button>
+        <Menu isOpen={sidebar?.isOpen} />
       </div>
-    </div>
+    </aside>
   );
 }
-
-<Sidebar />;
