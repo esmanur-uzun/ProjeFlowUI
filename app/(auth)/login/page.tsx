4efdb682fcfe84696e6c0543de2c6ea0ff19f556
esmanur-uzun <esmabitmez@gmail.com>
2024-09-18 16:09:27 +0300

    login page design completed

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,60 +1,25 @@
 "use client";
 import React, { useState } from "react";
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from "@/components/ui/command";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-
-const frameworks = [
-  {
-    value: "next.js",
-    label: "Next.js",
-  },
-  {
-    value: "sveltekit",
-    label: "SvelteKit",
-  },
-  {
-    value: "nuxt.js",
-    label: "Nuxt.js",
-  },
-  {
-    value: "remix",
-    label: "Remix",
-  },
-  {
-    value: "astro",
-    label: "Astro",
-  },
-];
+import loginImg from "../../public/login-img.jpg";
 
 const Login = () => {
-  const [open, setOpen] = useState(false);
-  const [value, setValue] = React.useState("");
-
   return (
-    <div className="flex min-h-screen  items-center justify-between p-52">
-      <Card className="w-[350px] grid-cols-2">
+    <div className="bg-slate-100 flex min-h-screen flex-col items-center justify-between p-48">
+      <Card className="w-[350px] bg-white">
+        <div className="justify-center items-center border-b-2 flex">
+          <Image src={loginImg} width={150} height={150} alt="" />
+        </div>
         <CardHeader>
           <CardTitle className="text-center">Sign in</CardTitle>
         </CardHeader>
@@ -62,69 +27,23 @@ const Login = () => {
           <form>
             <div className="grid w-full items-center gap-4">
               <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">User name</Label>
-                <Popover open={open} onOpenChange={setOpen}>
-                  <PopoverTrigger asChild>
-                    <Button
-                      variant="outline"
-                      role="combobox"
-                      aria-expanded={open}
-                      className=" "
-                    >
-                      {value
-                        ? frameworks.find(
-                            (framework) => framework.value === value
-                          )?.label
-                        : "Select framework..."}
-                      {/* <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" /> */}
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-[200px] p-0">
-                    <Command>
-                      <CommandInput
-                        placeholder="Search framework..."
-                        className="h-9"
-                      />
-                      <CommandList>
-                        <CommandEmpty>No framework found.</CommandEmpty>
-                        <CommandGroup>
-                          {frameworks.map((framework) => (
-                            <CommandItem
-                              key={framework.value}
-                              value={framework.value}
-                              onSelect={(currentValue) => {
-                                setValue(
-                                  currentValue === value ? "" : currentValue
-                                );
-                                setOpen(false);
-                              }}
-                            >
-                              {framework.label}
-                              {/* <CheckIcon
-                    className={cn(
-                      "ml-auto h-4 w-4",
-                      value === framework.value ? "opacity-100" : "opacity-0"
-                    )}
-                  /> */}
-                            </CommandItem>
-                          ))}
-                        </CommandGroup>
-                      </CommandList>
-                    </Command>
-                  </PopoverContent>
-                </Popover>
+                <Label htmlFor="name">User Name</Label>
+                <Input id="name" placeholder="User name..." />
               </div>
             </div>
           </form>
-          
+          <div className="mt-4">
+            <a className="text-xs underline" href="">
+              Forgotten your password?
+            </a>
+          </div>
         </CardContent>
         <CardFooter className="flex justify-end">
-          <Button className="w-2/4 bg-blue-950 text-white" variant="outline">
-            Login
+          <Button className="bg-red-600 text-slate-50" variant="outline">
+            Sign in
           </Button>
         </CardFooter>
       </Card>
-      <div className="w-full grid-cols-2 h-7 bg-black"></div>
     </div>
   );
 };
