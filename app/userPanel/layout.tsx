068a6547c5cd80c779ae076bfa0c8bc0799e2c6e
esmanur-uzun <esmabitmez@gmail.com>
2024-09-23 10:44:14 +0300

    worked on project list page

--- app/userPanel/layout.tsx
+++ app/userPanel/layout.tsx
@@ -18,7 +18,7 @@ export default function UserPanelLayout({
       <Sidebar />
       <main
         className={cn(
-          "min-h-[calc(100vh_-_56px)] bg-zinc-50 ps-5 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300",
+          "min-h-[calc(100vh_-_56px)] bg-zinc-50 ps-5 pe-5 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300",
           isOpen === false ? "lg:ml-[90px]" : "lg:ml-72"
         )}
       >
