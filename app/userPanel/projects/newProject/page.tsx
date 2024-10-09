ebf6386a3b4a22358944c99f837698aeb7350fbf
esmanur-uzun <esmabitmez@gmail.com>
2024-10-09 10:24:34 +0300

    worked on new project page

---
+++ app/userPanel/projects/newProject/page.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+const newProject = () => {
+  return (
+    <Card className="w-3/4 mx-auto mt-12">
+      <CardHeader>
+        <CardTitle>Create project</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="md:flex md:space-x-4 space-y-4 md:space-y-0">
+              <div className="flex flex-col space-y-1.5 md:w-1/2">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" placeholder="Name of your project" />
+              </div>
+              <div className="flex flex-col space-y-1.5 md:w-1/2">
+                <Label htmlFor="description">Description</Label>
+                <Input
+                  id="description"
+                  placeholder="Description of your project"
+                />
+              </div>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default newProject;
